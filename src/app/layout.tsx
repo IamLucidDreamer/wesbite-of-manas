c8effee0be23c62451ad71df7f538dd7ea43ec68
Manas Shukla <shuklamanasofficial@gmail.com>
2025-05-11 12:40:00 +0530

    Update : Layout Header Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-// import Header from '../components/Header';
+import Header from '../components/Header';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
