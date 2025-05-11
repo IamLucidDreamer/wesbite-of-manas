b4a4408f3ff19257b2c0e7cc3a6d1fe649cab575
Manas Shukla <shuklamanasofficial@gmail.com>
2025-05-11 12:40:00 +0530

    Update : Lamding Page changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import styles from "./page.module.scss";
 import { useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Preloader from "../components/Preloader";
-// import Landing from '../components/Landing';
-// import Projects from '../components/Projects';
-// import Description from '../components/Description';
-// import SlidingImages from '../components/SlidingImages';
-// import Contact from '../components/Contact';
+import Landing from '../components/Landing';
+import Description from '../components/Description';
+import Projects from '../components/Projects';
+import SlidingImages from '../components/SlidingImages';
+import Contact from '../components/Contact';
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -30,9 +30,9 @@ export default function Home() {
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
-      {/* <Landing />
+       <Landing />
       <Description />
-      <Projects />
+     {/*  <Projects />
       <SlidingImages />
       <Contact /> */}
     </main>
