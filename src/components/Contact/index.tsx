985d3b63493c7fa5d97a1b471ce2fbcc3fb52a1f
Manas Shukla <shuklamanasofficial@gmail.com>
2025-06-01 17:55:00 +0530

    Feat : Contact Section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -6,7 +6,7 @@ import { useScroll, motion, useTransform, useSpring } from 'framer-motion';
 import Magnetic from '../../common/Magnetic';
 
 export default function index() {
-    const container = useRef(null);
+    const container = useRef<HTMLElement>(null!);
     const { scrollYProgress } = useScroll({
         target: container,
         offset: ["start end", "end end"]
@@ -21,8 +21,10 @@ export default function index() {
     const time = `${hours}:${minutes < 10 ? '0' + minutes : minutes
     } ${hours >= 12 ? 'PM' : 'AM'}`;
     const currentYear = date.getFullYear();
+
+    const MotionDiv = motion.div as React.FC<any>;
     return (
-        <motion.div style={{y}} ref={container} className={styles.contact}>
+        <MotionDiv style={{y}} ref={container} className={styles.contact}>
             <div className={styles.body}>
                 <div className={styles.title}>
                     <span>
@@ -36,11 +38,11 @@ export default function index() {
                         <h2>Let's work</h2>
                     </span>
                     <h2>together</h2>
-                    <motion.div style={{x}} className={styles.buttonContainer}>
+                    <MotionDiv style={{x}} className={styles.buttonContainer}>
                         <Rounded  backgroundColor={"#334BD3"} className={styles.button}>
                             <p>Get in touch</p>
                         </Rounded>
-                    </motion.div>
+                    </MotionDiv>
                     <motion.svg style={{rotate, scale: 2}} width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
                     </motion.svg>
@@ -83,6 +85,6 @@ export default function index() {
                     </div>
                 </div>
             </div>
-        </motion.div>
+        </MotionDiv>
     )
 }
