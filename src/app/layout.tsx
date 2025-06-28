9301c580aa9293210ff35eb119c9e7b606bf9ef3
Manas Shukla <shuklamanasofficial@gmail.com>
2025-06-28 10:00:00 +0530

    Feat : NEW WEBSITE INITIAL

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+// import Header from '../components/Header';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Manas Shukla - Portfolio',
+  description: 'Portfolio of Manas Shukla, a web developer specializing in modern web technologies.',
+}
+
+export default function RootLayout({ children } : { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <Header /> */}
+        {children}
+      </body>
+    </html>
+  )
+}
