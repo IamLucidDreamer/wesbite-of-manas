274a0ba995d74c11546bff1c6ec8e8f567d5ee6a
Manas Shukla <shuklamanasofficial@gmail.com>
2025-07-10 06:18:22 +0530

    Feat : Slider UI Update

--- src/components/SlidingImages/index.tsx
+++ src/components/SlidingImages/index.tsx
@@ -1,93 +1,102 @@
-import { useRef } from 'react';
-import { useScroll, useTransform, motion } from 'framer-motion';
-import styles from './style.module.scss';
-import Image from 'next/image';
+import { useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
+import styles from "./style.module.scss";
+import Image from "next/image";
 
 const slider1 = [
-    {
-        color: "#e3e5e7",
-        src: "c2.jpg"
-    },
-    {
-        color: "#d6d7dc",
-        src: "decimal.jpg"
-    },
-    {
-        color: "#e3e3e3",
-        src: "funny.jpg"
-    },
-    {
-        color: "#21242b",
-        src: "google.jpg"
-    }
-]
+  {
+    color: "#d6d7dc",
+    src: "landing_page/ulearn.png",
+  },
+  {
+    color: "#e3e5e7",
+    src: "ionic_landing_hero.png",
+  },
+  {
+    color: "#e3e3e3",
+    src: "unacademy-image.png",
+  },
+  {
+    color: "#f4f4f4",
+    src: "landing_page/graphy.png",
+  },
+];
 
 const slider2 = [
-    {
-        color: "#d4e3ec",
-        src: "maven.jpg"
-    },
-    {
-        color: "#e5e0e1",
-        src: "panda.jpg"
-    },
-    {
-        color: "#d7d4cf",
-        src: "powell.jpg"
-    },
-    {
-        color: "#e1dad6",
-        src: "wix.jpg"
-    }
-]
+  {
+    color: "#e5e0e1",
+    src: "landing_page/the-motivate-kids.png",
+  },
+  {
+    color: "#d4e3ec",
+    src: "landing_page/aamdhane-new.png",
+  },
+  {
+    color: "#d7d4cf",
+    src: "landing_page/the-fence-sitter.png",
+  },
+  {
+    color: "#e1dad6",
+    src: "landing_page/nature-good-life.png",
+  },
+];
 
 export default function SlidingImage() {
+  const container = useRef<HTMLDivElement>(null!);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
 
-    const container = useRef<HTMLDivElement>(null!);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ["start end", "end start"]
-    })
+  const x1 = useTransform(scrollYProgress, [0, 1], [0, 150]);
+  const x2 = useTransform(scrollYProgress, [0, 1], [0, -150]);
+  const height = useTransform(scrollYProgress, [0, 0.9], [50, 0]);
+  // eslint-disable-next-line
+  const MotionDiv = motion.div as React.FC<any>;
 
-    const x1 = useTransform(scrollYProgress, [0, 1], [0, 150])
-    const x2 = useTransform(scrollYProgress, [0, 1], [0, -150])
-    const height = useTransform(scrollYProgress, [0, 0.9], [50, 0])
-    // eslint-disable-next-line
-    const MotionDiv = motion.div as React.FC<any>;
-
-    return (
-        <div ref={container} className={styles.slidingImages}>
-            <MotionDiv style={{x: x1}} className={styles.slider}>
-                    {
-                        slider1.map( (project, index) => {
-                            return <div key={index} className={styles.project} style={{backgroundColor: project.color}} >
-                                <div className={styles.imageContainer}>
-                                    <Image 
-                                    fill={true}
-                                    alt={"image"}
-                                    src={`/images/${project.src}`}/>
-                                </div>
-                            </div>
-                        })
-                    }
-                </MotionDiv>
-                <MotionDiv style={{x: x2}} className={styles.slider}>
-                    {
-                        slider2.map( (project, index) => {
-                            return <div key={index} className={styles.project} style={{backgroundColor: project.color}} >
-                                <div key={index} className={styles.imageContainer}>
-                                    <Image 
-                                    fill={true}
-                                    alt={"image"}
-                                    src={`/images/${project.src}`}/>
-                                </div>
-                            </div>
-                        })
-                    }
-                </MotionDiv>
-                <MotionDiv style={{height}} className={styles.circleContainer}>
-                    <div className={styles.circle}></div>
-                </MotionDiv>
-        </div>
-    )
+  return (
+    <div ref={container} className={styles.slidingImages}>
+      <MotionDiv style={{ x: x1 }} className={styles.slider}>
+        {slider1.map((project, index) => {
+          return (
+            <div
+              key={index}
+              className={styles.project}
+              style={{ backgroundColor: project.color }}
+            >
+              <div className={styles.imageContainer}>
+                <Image
+                  fill={true}
+                  alt={"image"}
+                  src={`/images/${project.src}`}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </MotionDiv>
+      <MotionDiv style={{ x: x2 }} className={styles.slider}>
+        {slider2.map((project, index) => {
+          return (
+            <div
+              key={index}
+              className={styles.project}
+              style={{ backgroundColor: project.color }}
+            >
+              <div key={index} className={styles.imageContainer}>
+                <Image
+                  fill={true}
+                  alt={"image"}
+                  src={`/images/${project.src}`}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </MotionDiv>
+      <MotionDiv style={{ height }} className={styles.circleContainer}>
+        <div className={styles.circle}></div>
+      </MotionDiv>
+    </div>
+  );
 }
